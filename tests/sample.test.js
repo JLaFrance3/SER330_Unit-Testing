d0b3391bb8b04ca9628782280c75c4b2f88be0e6
Jean <jlafranceiii@proton.me>
2026-04-22 14:21:11 -0400

    Sample test

---
+++ tests/sample.test.js
@@ -0,0 +1,11 @@
+const {describe, test} = require('@jest/globals')
+
+describe('Person', () => {
+    test('should return a new person', () => {
+        // Arrange
+
+        // Act
+        console.log("Hello World")
+        // Assert
+    })
+})
